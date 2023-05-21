b6c52cf9ea155e72e87ca34b06e74aa0e4e98df4
arafatyamin <mdh631987@gmail.com>
2023-05-21 01:45:44 -0700

    change somthing

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const http = require('http');
 const cors = require('cors');
 const PORT = 5000 || process.env.PORT;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const Msg = require('./models/message');
 
 app.use(cors());
-
-
 
 const httpServer = http.createServer(app);
 const {Server} = require("socket.io");
@@ -22,85 +21,36 @@ const io = new Server(httpServer, {
 
 // Connect to MongoDB
 
-const uri = `mongodb+srv://dentalCare:LQ8WJAYSKIuFrNkb@cluster0.o0lhbrs.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://dentalCare:LQ8WJAYSKIuFrNkb@cluster0.o0lhbrs.mongodb.net/message-database?retryWrites=true&w=majority`;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log('connected')
+}).catch(err => console.log(err))
 
-const messagesCollection = client.db("chatApplication").collection("messages");
+// const messagesCollection = client.db("chatApplication").collection("messages");
 
 
 io.on("connection", (socket) => {
     console.log("new user connected to our app");
 
-// send message from the mongodb collection
-  socket.on("newMessage", function(msg){
-    messagesCollection.insertOne({msg}, (err) => {
-          if (err) return console.error(err);
-          console.log("Message saved to MongoDB", msg);
-        })
-        io.emit("sendMessage", msg)
+    Msg.find().then(result => {
+      socket.emit('output-messages', result)
   })
-
-  // // get chat from the mongodb collection
-  // messagesCollection.find().toArray(function(err, {res}) {
-  //   if (err){ throw err;}
-  //   console.log('Messages fetched from database:', res);
-  //   // emit the messages
-  //   socket.emit('recivedMessage', res);
-  // })
-
-  // send all messages to the new client
-  // messagesCollection.find((err, messages) => {
-  //   if (err) return console.error(err);
-  //   socket.emit('messages', messages);
-  // });
-
-  messagesCollection.find().toArray((err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      // Send the data to the client(s)
-      socket.emit('myData', results);
-    }
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-    // // Emit all messages to client on connection
-    //     messagesCollection.find({}).toArray((err, msg) => {
-    //         if (err) return console.error(err);
-    //         console.log(msg)
-    //         socket.emit("allMessages", 'my name is yamin');
-    //         socket.emit('mymessage', )
-            
-    //     })
-
-        
-    // // Receive new message from client
-    // socket.on("newMessage", (message) => {
-    //     messagesCollection.insertOne(message, (err) => {
-    //     if (err) return console.error(err);
-
-    //     console.log("Message saved to MongoDB", message);
-    //     });
-
-
-    
-
-    
 
     // Disconnect listener
   socket.on("disconnect", () => {
     console.log("A user has disconnected");
   });
+
+  socket.on('chatmessage', msg => {
+    console.log(msg)
+    const message = new Msg({ msg });
+    message.save().then((data) => {
+      console.log(data)
+      io.emit('message', data)
+  })
+
+
+})
 
 });
 
